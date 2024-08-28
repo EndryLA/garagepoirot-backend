483ddae1bed2cad7cb3486b53244c212b67c6d21
Endry LUNDY ANDRES <clansdoli@gmail.com>
2024-08-28 18:34:33 +0200

    added connection ssl to db

--- database/connect.js
+++ database/connect.js
@@ -1,5 +1,6 @@
 import mysql from 'mysql2'
 import dotenv from 'dotenv'
+import fs from 'fs'
 dotenv.config()
 
 const database = mysql.createConnection({
@@ -7,7 +8,12 @@ const database = mysql.createConnection({
     user:process.env.DB_USERNAME,
     password:process.env.DB_PASSWORD,
     database:process.env.DB_NAME,
-    port: process.env.DB_HOST
+    port: process.env.DB_PORT,
+    ssl: {
+        rejectUnauthorized: true,
+        ca: fs.readFileSync('./config/ca.pem')
+
+    }
 })
 
 database.connect((error) => {
--- index.js
+++ index.js
@@ -14,12 +14,21 @@ dotenv.config()
 const app = express();
 
 app.use(express.json())
-app.use(helmet())
+
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Headers',"Origin, Authorization, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version");
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    next()
+});
+
 app.use(cors({
     origin: '*',
     credentials: true,
     optionSuccessStatus: 200,
 }));
+
 
 
 app.use('/api/services',servicesRouter)
